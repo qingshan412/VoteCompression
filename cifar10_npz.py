1ae8a82f13563c5a8c352c4e5db4e37cfaa34be5
qingshan412 <qingshan412@hotmail.com>
2018-04-20 11:30:53 -0400

    data

--- cifar10_npz.py
+++ cifar10_npz.py
@@ -1,53 +1,77 @@
 """Transfer CIFAR dataset to npz format.
 """
-import os, cPickle
+import os, cPickle, argparse
 from glob import glob
-# import tensorflow as tf
+import numpy as np
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-d","--DataDir", type=str,
+                    help="directory for data",
+                    default = '../../data')
+parser.add_argument("-m","--Mode", type=str,
+                    help="train or test or valid",
+                    default = 'train')
+parser.add_argument("-s","--DataSet", type=str,
+                    help="dataset's name",
+                    default = 'cifar10')
+
+args = parser.parse_args()
+DataPath = args.DataDir
+Mode = args.Mode
+DataSet = args.DataSet
 
 def unpickle(file):
     with open(file, 'rb') as fo:
         dict = cPickle.load(fo)
     return dict
 
-def save2npz(data_path, batch_size, mode, dataset='cifar10'):
+def save2npz(data_path, mode, dataset='cifar10'):
   """Transfer CIFAR image and labels to npz format.
 
   Args:
     dataset: Either 'cifar10' or 'cifar100'.
     data_path: Filename for data.
-    batch_size: Input batch size.
     mode: Either 'train' or 'eval'.
   Returns:
     None
   Store:
     images and labels in the format of npz
-    images: [batch_size, image_size, image_size, 3]
-    labels: [batch_size, num_classes]
+    features: [100000, image_size, image_size, 3]
+    labels: [100000, num_classes]
   Raises:
     ValueError: when the specified dataset is not supported.
   """
   image_size = 32
-  if dataset 1= 'cifar10' and dataset != 'cifar100':
+  channel_size = 3
+  
+  if dataset != 'cifar10' and dataset != 'cifar100':
     raise ValueError('Not supported dataset %s', dataset)
 
-  depth = 3
-  image_bytes = image_size * image_size * depth
-  record_bytes = label_bytes + label_offset + image_bytes
-
   if mode == 'train':
-    data_files = glob(os.path.join(data_path, 'data*'))
+    data_files = glob(os.path.join(data_path, dataset, 'data*'))
   else:
-    data_files = glob(os.path.join(data_path, 'test*'))
-  file_queue = tf.train.string_input_producer(data_files, shuffle=True)
-  # Read examples from files in the filename queue.
-  reader = tf.FixedLengthRecordReader(record_bytes=record_bytes)
-  _, value = reader.read(file_queue)
+    data_files = glob(os.path.join(data_path, dataset, 'test*'))
+  
+  for i in range(len(data_files)):
+    data_tmp = unpickle(data_files[i])
+    data = data_tmp['data']#10000x3072
+    labels_tmp = np.array(data_tmp['labels'])#10000
 
-  # Convert these examples to dense labels and processed images.
-  record = tf.reshape(tf.decode_raw(value, tf.uint8), [record_bytes])
-  label = tf.cast(tf.slice(record, [label_offset], [label_bytes]), tf.int32)
-  # Convert from string to [depth * height * width] to [depth, height, width].
-  depth_major = tf.reshape(tf.slice(record, [label_offset + label_bytes], [image_bytes]),
-                           [depth, image_size, image_size])
-  # Convert from [depth, height, width] to [height, width, depth].
-  image = tf.cast(tf.transpose(depth_major, [1, 2, 0]), tf.float32)
+    m = data.shape[0]
+    data = data.reshape((m,channel_sizes,image_size,images_size))
+    features_tmp = np.transpose(data,(0,2,3,1))
+    
+    if i < 1:
+      features = features_tmp
+      labels = labels_tmp
+    else:
+      features = np.vstack((features, features_tmp))
+      labels = np.vstack((labels, labels_tmp))
+  
+  np.savez(os.path.join(data_path, dataset, 'feature_'+ mode +'.npz'), 
+    features = features, labels = labels)
+
+if __name__ == "__main__":
+  save2npz(data_path=DataPath, mode=Mode, dataset=DataSet):
+  
