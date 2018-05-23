9c399d76b6292ac7a054498ef854e0822b135bf1
qingshan412 <qingshan412@hotmail.com>
2018-05-23 08:54:31 -0400

    update

--- Dmodels.py
+++ Dmodels.py
@@ -33,7 +33,7 @@ def _variable_on_cpu(name, shape, initializer):
         Variable Tensor
     """
     with tf.device('/cpu:0'):
-        dtype = tf.float16 if FLAGS.use_fp16 else tf.float32
+        dtype = tf.float32#tf.float16 if FLAGS.use_fp16 else tf.float32
         var = tf.get_variable(name, shape, initializer=initializer, dtype=dtype)
     return var
 
