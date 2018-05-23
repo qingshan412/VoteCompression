479fcbf73e3bd7a5fab8b19b6d167eea35d95f79
qingshan412 <qingshan412@hotmail.com>
2018-05-23 09:13:54 -0400

    update

--- Ntrain.py
+++ Ntrain.py
@@ -61,8 +61,8 @@ def main(_):
         images = data["features"]
         labels =  data["labels"]
 
-    print('levels size:')
-    print(levels.shape)
+    print('labels size:')
+    print(labels.shape)
 
     level_images, level_labels = level_assign(images, labels, levels)
     
